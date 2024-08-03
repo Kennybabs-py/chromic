7e9ce2dbaf2d5af7c71303bd17f312abf05e08cb
Kennybabs-py <babalolakehinde74@gmail.com>
2024-08-03 11:53:25 +0100

    markup and styling done; js animation in progress

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const logger = require("morgan");
 const methodOverride = require("method-override");
+const prismic = require("@prismicio/client");
+const fetch = require("node-fetch");
 
 const app = express();
 const port = 3000;
-
-const prismic = require("@prismicio/client");
-const fetch = require("node-fetch");
 
 const repoName = "chromic";
 const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
@@ -21,19 +20,14 @@ app.use(errorHandler());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
+app.use(express.static(path.join(__dirname, "public")));
 
-/** The `routes` property is your route resolver. It defines how you will
+/**
+ ** The `routes` property is your route resolver. It defines how you will
  ** structure URLs in your project. Update the types to match the Custom
- **Types in your project, and edit the paths to match the routing in your
- **project.
+ ** Types in your project, and edit the paths to match the routing in your
+ ** project.
  */
-
-const routes = [
-  {
-    type: "page",
-    path: "/:uid",
-  },
-];
 
 //* Creating a client that connects prismic repo with express paths
 const client = prismic.createClient(repoName, {
@@ -41,6 +35,25 @@ const client = prismic.createClient(repoName, {
   accessToken,
   // routes,
 });
+
+function handleLinkResolver(doc) {
+  if (doc.type === "produc") {
+    return `/detail/${doc.slug}`;
+  }
+  if (doc.type === "about") {
+    return `/about`;
+  }
+  if (doc.type === "collections") {
+    return `/collections`;
+  }
+  console.log(doc);
+  return "/";
+}
+
+function handleIndexResolver(index) {
+  let arr = ["One", "Two", "Three", "Four", "Five"];
+  return arr[index];
+}
 
 //* This commented out code was used in the previous version of prismic & express integration
 //! Not to be used, but as a reference
@@ -53,28 +66,13 @@ const client = prismic.createClient(repoName, {
 //   next();
  });
  */
-
-function handleLinkResolver(doc) {
-  // if (doc.type === "blog_post") {
-  //   const date = new Date(doc.first_publication_date);
-  //   return `/${date.getFullYear()}/${doc.uid}`;
-  // }
-  return "/";
-}
-
-function handleIndexResolver(index) {
-  let arr = ["One", "Two", "Three", "Four", "Five"];
-
-  return arr[index];
-}
-
 app.use((req, res, next) => {
-  // res.locals.ctx = {
-  //   prismic,
-  //   endpoint: process.env.PRISMIC_ENDPOINT,
-  //   linkResolver: handleLinkResolver,
-  //   indexResolver: handleIndexResolver,
-  // };
+  res.locals.ctx = {
+    prismic,
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+    indexResolver: handleIndexResolver,
+  };
   res.locals.prismic = prismic;
   res.locals.endpoint = process.env.PRISMIC_ENDPOINT;
   res.locals.linkResolver = handleLinkResolver;
@@ -87,43 +85,37 @@ app.use((req, res, next) => {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-//*  Home page
-app.get("/", async (req, res) => {
-  const meta = await client.getSingle("metadata");
-  const preloader = await client.getSingle("preloader");
+async function handleDefaults(api) {
+  const meta = await api.getSingle("metadata");
+  const navigation = await api.getSingle("navigation");
+  const preloader = await api.getSingle("preloader");
+  return { meta, navigation, preloader };
+}
 
-  res.render("pages/home", { meta, preloader });
+//* Home page
+app.get("/", async (req, res) => {
+  const home = await client.getSingle("home");
+  const defaults = await handleDefaults(client);
+  const collections = await client.getAllByType("collection", {
+    fetchLinks: "produc.image",
+  });
+  res.render("pages/home", { ...defaults, home, collections });
 });
 
 //* About page
 app.get("/about", async (req, res) => {
   try {
     const about = await client.getSingle("about");
-    const meta = await client.getSingle("metadata");
-    const preloader = await client.getSingle("preloader");
-
-    res.render("pages/about", { about, meta, preloader });
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-  }
-});
-
-//* Collection detail page
-app.get("/detail/:uid", async (req, res) => {
-  const { uid } = req.params;
-
-  try {
-    const meta = await client.getSingle("metadata");
-    const product = await client.getByUID("produc", uid, {
-      fetchLinks: "collection.title",
-    });
-    const preloader = await client.getSingle("preloader");
-
-    res.render("pages/detail", {
-      product,
-      preloader,
-      meta,
-    });
+    const defaults = await handleDefaults(client);
+    // console.log(
+    //   about.data.body
+    //     .filter(
+    //       (item) =>
+    //         item.slice_type === "content" && item.primary.type === "Left",
+    //     )
+    //     .map((item) => item.primary.description),
+    // );
+    res.render("pages/about", { ...defaults, about });
   } catch (error) {
     console.log(`Error: ${error.message}`);
   }
@@ -132,20 +124,33 @@ app.get("/detail/:uid", async (req, res) => {
 //* Collections page
 app.get("/collections", async (req, res) => {
   try {
-    const meta = await client.getSingle("metadata");
+    const defaults = await handleDefaults(client);
     const home = await client.getSingle("home");
     const collections = await client.getAllByType("collection", {
       fetchLinks: "produc.image",
     });
-    const preloader = await client.getSingle("preloader");
-
-    console.log(preloader);
-
     res.render("pages/collections", {
+      ...defaults,
       collections,
       home,
-      preloader,
-      meta,
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+});
+
+//* Collection detail page
+app.get("/detail/:uid", async (req, res) => {
+  const { uid } = req.params;
+  try {
+    const defaults = await handleDefaults(client);
+    const product = await client.getByUID("produc", uid, {
+      fetchLinks: "collection.title",
+    });
+
+    res.render("pages/detail", {
+      ...defaults,
+      product,
     });
   } catch (error) {
     console.log(`Error: ${error.message}`);
